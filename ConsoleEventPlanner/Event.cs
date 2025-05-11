189fe8b4b17003973401a63b0345369a8ce9bb85
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-05-11 14:54:43 +0200

    implement console menu

--- ConsoleEventPlanner/Event.cs
+++ ConsoleEventPlanner/Event.cs
@@ -10,6 +10,7 @@ namespace ConsoleEventPlanner
     {
         private string Title { get; set; }
         private DateTime Date { get; set; }
+
         public Event(string input)
         {
             string[] parts = input.Split(";");
@@ -20,10 +21,12 @@ namespace ConsoleEventPlanner
                 Date = DateTime.Parse(parts[2]);
             }
         }
+
         public string getTitle()
         {
             return Title;
         }
+
         public DateTime getDate()
         {
             return Date;
--- ConsoleEventPlanner/Program.cs
+++ ConsoleEventPlanner/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        while (true)
+        {
+            Console.WriteLine("============== EVENT PLANNER ===============");
+            Console.WriteLine("Enter a command in the following format:");
+            Console.WriteLine("  EVENT;event name;YYYY-MM-DD");
+            Console.WriteLine("  LIST   - displays all stored events");
+            Console.WriteLine("  STATS  - shows event statistics by date");
+            Console.WriteLine("  END    - exits the program");
+            Console.WriteLine("===========================================");
+            string input = Console.ReadLine();
+            if (input.ToUpper() == "END")
+            {
+                return;
+            }
+        }
     }
 }
